e260efb05d542786a6e379c629939532fcf6936a
xiepg <xiepg@meixing.com>
2024-10-10 16:13:49 +0800

    let's go

--- exercises/07_threads/09_bounded/src/lib.rs
+++ exercises/07_threads/09_bounded/src/lib.rs
@@ -1,40 +1,55 @@
 // TODO: Convert the implementation to use bounded channels.
 use crate::data::{Ticket, TicketDraft};
 use crate::store::{TicketId, TicketStore};
-use std::sync::mpsc::{Receiver, Sender};
+use std::sync::mpsc::{Receiver, RecvError, SendError, Sender, SyncSender};
+use std::sync::mpsc::sync_channel;
+
+
 
 pub mod data;
 pub mod store;
 
 #[derive(Clone)]
 pub struct TicketStoreClient {
-    sender: todo!(),
+    sender: SyncSender<Command>,
 }
 
 impl TicketStoreClient {
-    pub fn insert(&self, draft: TicketDraft) -> Result<TicketId, todo!()> {
-        todo!()
+    pub fn insert(&self, draft: TicketDraft) -> Result<TicketId, RecvError> {
+        let (response_channel, response_receiver) = std::sync::mpsc::channel();
+        self.sender.send(Command::Insert {
+            draft,
+            response_channel,
+        }).expect("panic");
+        response_receiver.recv()
     }
 
-    pub fn get(&self, id: TicketId) -> Result<Option<Ticket>, todo!()> {
-        todo!()
+    pub fn get(&self, id: TicketId) -> Result<Option<Ticket>, RecvError> {
+        let (response_channel, response_receiver) = std::sync::mpsc::channel();
+        self.sender.send(Command::Get {
+            id,
+            response_channel,
+        }).expect("panic");
+        response_receiver.recv()
     }
 }
 
 pub fn launch(capacity: usize) -> TicketStoreClient {
-    todo!();
+    let (sender, receiver) = sync_channel(capacity);
     std::thread::spawn(move || server(receiver));
-    todo!()
+    TicketStoreClient {
+        sender
+    }
 }
 
 enum Command {
     Insert {
         draft: TicketDraft,
-        response_channel: todo!(),
+        response_channel: Sender<TicketId>,
     },
     Get {
         id: TicketId,
-        response_channel: todo!(),
+        response_channel: Sender<Option<Ticket>>,
     },
 }
 
@@ -47,14 +62,14 @@ pub fn server(receiver: Receiver<Command>) {
                 response_channel,
             }) => {
                 let id = store.add_ticket(draft);
-                todo!()
+                response_channel.send(id).expect("panic");
             }
             Ok(Command::Get {
                 id,
                 response_channel,
             }) => {
                 let ticket = store.get(id);
-                todo!()
+                response_channel.send(ticket.cloned()).expect("panic");
             }
             Err(_) => {
                 // There are no more senders, so we can safely break
